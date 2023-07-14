aa9a4ddce2f0f045fa8efd0db6e6174a2384163b
christopherparker1923 <christmas1923@gmail.com>
2023-07-14 11:53:00 -0400

    Add project files.

---
+++ object-oriented-practice/Data/DataContextEF.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using object_oriented_practice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace object_oriented_practice.Data
+{
+    internal class DataContextEF :DbContext
+    {
+
+        private IConfiguration _config;
+
+        public DataContextEF(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public DbSet<Computer>? Computer { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(_config.GetConnectionString("DefaultConnection"),
+                    options => options.EnableRetryOnFailure());
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("TutorialAppSchema");
+            modelBuilder.Entity<Computer>().HasKey(c => c.ComputerId);
+                /*.ToTable("Computer", "TutorialAppSchema");*/
+        }
+
+    }
+}
---
+++ object-oriented-practice/Program.cs
@@ -0,0 +1,76 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using object_oriented_practice.Data;
+using object_oriented_practice.Models;
+using System.Data;
+using System.Text.Json;
+
+namespace HelloWorld
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            DataContextDapper dapper = new DataContextDapper(config);
+
+            string computersJSON = File.ReadAllText("Computers.json");
+
+            //Console.WriteLine(computersJSON);
+
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+
+            IEnumerable<Computer>? computers = System.Text.Json.JsonSerializer.Deserialize<IEnumerable<Computer>>(computersJSON, options);
+
+            if(computers != null)
+            {
+                foreach(Computer computer in computers)
+                {
+                    Console.WriteLine(computer.Motherboard);
+                    string sql = @"INSERT INTO TutorialAppSchema.Computer (
+                        Motherboard,
+                        HasWifi,
+                        HasLTE,
+                        ReleaseDate,
+                        Price,
+                        VideoCard
+                        ) VALUES ('"
+                        + EscapeSingleQuote(computer.Motherboard)
+                        + "','" + computer.HasWifi
+                        + "','" + computer.HasLTE
+                        + "','" + computer.ReleaseDate
+                        + "','" + computer.Price
+                        + "','" + EscapeSingleQuote(computer.VideoCard)
+                        + "')";
+
+                    dapper.ExecuteSql(sql);
+                }
+            }
+
+            string computersCopy = System.Text.Json.JsonSerializer.Serialize(computers, options);
+
+            File.WriteAllText("computersCopySystem.txt", computersCopy);
+
+
+        }
+
+        static string EscapeSingleQuote(string input)
+        {
+            string output = input.Replace("'", "''");
+
+            return output;
+        }
+    }
+}
+
+
